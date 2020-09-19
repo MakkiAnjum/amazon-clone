dd33a9ce0617e6db390a8fed6e94f98e24041c95
MakkiAnjum <muhammadmakkianjum@yahoo.om>
2020-09-19 23:02:46 +0500

    Homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     // BEM
     <div className="app">
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
---
+++ src/components/Product.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Product.css";
+
+function Product({ id, title, image, price, rating }) {
+  const addToBasket = () => {};
+
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
+        </div>
+      </div>
+
+      <img src={image} alt="" />
+
+      <button onClick={addToBasket}>Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
